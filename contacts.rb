955994b0ba007d7399f386523924530e6f6dd290
Sundar Jayabose <sundarjayabose@Sundars-MacBook-Pro.local>
2023-03-02 09:14:27 -0500

    feature: add user + flash message

--- contacts.rb
+++ contacts.rb
@@ -15,7 +15,6 @@ end
 configure do
   enable :sessions
   # set :session_secret, 'secret'
-
 end
 
 # Loads contacts from file at start of sesison, else returns contacts from session
@@ -24,21 +23,41 @@ def contacts
   session[:contacts]
 end
 
+# Display contacts
 get "/" do
   @contacts_list = contacts
   erb :contacts
 end
 
+# Display edit contact form
 get "/edit/:id" do
   @id = params[:id]
   @contact = session[:contacts][@id]
   erb :edit
 end
 
+# Handle edit contact form
 post "/edit/:id" do
   contact_record = params.keys.each_with_object({}) do |key, obj|
     obj[key] = params[key] unless key == "id"
   end
   session[:contacts][params[:id]] = contact_record
+  redirect "/"
+end
+
+# Display new contact form
+get "/new" do 
+  erb :new
+end
+
+# Handle new contact form
+# post "/new" do |name, email, phone, notes|
+post "/new" do
+  # %w(foo bar)
+  keys = %w(name email phone notes)
+  values = [params[:name], params[:email], params[:phone], params[:notes]]
+  contact_record = keys.zip(values).to_h
+  session[:contacts][params[:email]] = contact_record
+  session[:message] = "New contact created."
   redirect "/"
 end
